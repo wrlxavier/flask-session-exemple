0e3d4a83b835ce939c5dc2bd30f408037b6acc70
wrlxavier <warleyxavier.fernandes@gmail.com>
2023-02-28 21:50:10 -0300

    First commit

---
+++ main.py
@@ -0,0 +1,35 @@
+from flask import Flask, session, request, redirect, url_for
+
+app = Flask(__name__)
+
+app.secret_key = b'12345'
+
+@app.route('/', methods=['GET'])
+def index():
+
+    if 'username' in session:
+        return f'Logged in as {session["username"]}'
+    return 'You are not logged in'
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+
+if __name__ == '__main__':
+    app.run()
